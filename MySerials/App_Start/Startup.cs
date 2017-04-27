c2a39d992056b0a1dfd8b08cf837463e7560ecb6
KostyaTymko <kostyatymko@gmail.com>
2017-04-27 10:35:58 +0300

    Authentication step 1

---
+++ MySerials/App_Start/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySerials.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(MySerials.App_Start.Startup))]
+namespace MySerials.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<ApplicationContext>(
+                ApplicationContext.Create
+                );
+            app.CreatePerOwinContext<UserManager>(
+                UserManager.Create
+                );
+            app.UseCookieAuthentication(
+                new CookieAuthenticationOptions()
+                {
+                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                    LoginPath = new PathString("/Account/Login")
+                }
+                );
+        }
+    }
+}
---
+++ MySerials/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MySerials.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext()
+            : base("SerialContext") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
